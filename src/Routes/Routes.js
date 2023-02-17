7d75421556117b5593b9a46b6a5d70875174fcb1
sabbir1020 <gazisabbir924@gamil.com>
2023-02-17 08:05:19 +0600

    first commit

---
+++ src/Routes/Routes.js
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: "",
+      },
+    ],
+  },
+]);
---
+++ src/layout/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Footer/Footer";
+import NavBar from "../Pages/NavBar/NavBar";
+
+const Main = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
